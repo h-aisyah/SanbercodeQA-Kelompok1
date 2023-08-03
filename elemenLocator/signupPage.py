3fc9c7a961bf186c03fa45e3e98e5d8f1b2ad044
h-aisyah <yanghejo1@gmail.com>
2023-08-03 12:36:59 +0700

    menambah folder Elemen Locator POM

---
+++ elemenLocator/signupPage.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+
+class signupPageClass():
+	fname = "firstname"
+	lname = "lastname"
+	e_addrs = "email_address"
+	pssw = "password"
+	pssw_confrm = "password-confirmation"
+	btn_save = "action.submit.primary"
+	
